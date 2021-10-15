799cc2a58f1550980eeb1147781408348240fb74
JosephCheney <che20001@byui.edu>
2021-10-15 10:19:04 -0600

    re formatted the director

--- 05-jumper/jumper/game/director.py
+++ 05-jumper/jumper/game/director.py
@@ -20,21 +20,34 @@ class Director():
         """
         self.words = Words()
 
-
+        self.keep_playing = True
         self.is_hardmode = False
         self.wrong_guesses = 0
 
     # maybe add a function that asks for hardmode? or a custom wordbank filename?
 
     def start_game(self):
-        self.words.fetch_word()
+        print ("This is essentally a game of hang man. If you guess wrong the parachuter will loose part of his parachute and you may eventually die.")
+        
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+        
         # print(f"{self.words.current_word}") # this is for debugging
     
-    def get_word(self):
+    def get_inputs(self):
+        self.words.fetch_word()
+
+
+    def do_updates(self):
         pass
 
-    def get_input(self):
+
+    def do_outputs(self):
         pass
+   
 
     # I dont know what else this class needs. good luck
 
