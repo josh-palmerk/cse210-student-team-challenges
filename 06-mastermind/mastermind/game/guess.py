6714012897f8d03d065243122a325f7cc0da6434
Josh Palmer <josh_palmerk@byui.edu>
2021-10-20 13:20:16 -0600

    finished user input functions and accessor

--- 06-mastermind/mastermind/game/guess.py
+++ 06-mastermind/mastermind/game/guess.py
@@ -1,22 +1,35 @@
 class Guess():
     """
-    gets guess and verifies input
+    Handles everything related to a guess.
+    To use properly, run a while loop that depends on the 
+    verify_input outcome, with user_input inside the loop.
+    When loop is complete, run guess_to_list, then get your 
+    result with the get_guess accessor method.
+    Methods:
+        user_input(): Prompts user and saves unfiltered guess
+        verify_input(): Returns True if guess meets criteria
+        guess_to_list(): Final processing piece for guess. Self explanatory
+        get_guess(): Accessor method for "finished" guess
     """
     def __init__(self):
         """
         Class constructor
         """
         self._unfiltered_guess = ""
+        self.str_guess = ""
         self._guess = ""
 
     def get_guess(self):
+        """
+        Accessor method for _guess
+        """
         return self._guess
 
     def user_input(self):
         """
-        saves guess into private var
+        Prompts user and saves guess into private attribute for verifying and processing
         """
-        self._unfiltered_guess = input(f"What is your guess? ")
+        self._unfiltered_guess = input("What is your guess? ") # use console.read() ?
 
     def verify_input(self):
         """
@@ -24,8 +37,14 @@ class Guess():
         returns bool
         """
         self._unfiltered_guess = self._unfiltered_guess.strip()
-        if len(self._unfiltered_guess) == 4:
-            self._guess = self._unfiltered_guess
+        if len(self._unfiltered_guess) == 4 and self._unfiltered_guess.isdecimal():
+            self._str_guess = self._unfiltered_guess
             return True
         else:
             return False
+
+    def guess_to_list(self):
+        """
+        Turns _guess into a list with the contents of the _str_guess string.
+        """
+        self._guess = list(self._str_guess)
