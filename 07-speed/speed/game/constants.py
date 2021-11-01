193effb4a62b860e9ac7248dc1f58513f61ded7c
Josh Palmer <josh_palmerk@byui.edu>
2021-11-01 10:51:52 -0600

    word.py complete (?)

--- 07-speed/speed/game/constants.py
+++ 07-speed/speed/game/constants.py
@@ -6,3 +6,6 @@ FRAME_LENGTH = 0.08
 STARTING_WORDS = 5
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
+
+DEFAULT_WORD_HEIGHT = 20
+DEFAULT_WORD_SPEED = -1
---
+++ 07-speed/speed/game/word.py
@@ -0,0 +1,25 @@
+from random import randint
+from game import constants
+from game.actor import Actor
+from game.point import Point
+
+class Word(Actor):
+    def __init__(self, word):
+        super().__init__()
+        
+        self._word = word
+        self.set_width = len(self._word) * 6 # test value for expanded text box for diff word lengths
+        self._points = len(self._word) # words
+        self.set_text(f"{self._word}") # word text
+
+        self.set_height = constants.DEFAULT_WORD_HEIGHT
+        self.set_position(Point(constants.MAX_X, randint(0, constants.MAX_Y)))
+        self.set_velocity(Point(constants.DEFAULT_WORD_SPEED, 0))
+
+    def get_points(self):
+        return self._points
+
+    def get_word(self):
+        """ in case we need it i guess """
+        return self._word
+        
